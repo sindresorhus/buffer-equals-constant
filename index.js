08518f12907951b97738f6fbc7a5429057ab089d
Michael Mayer <michael@schnittstabil.de>
2015-10-19 20:12:57 +0700

    Close #4 PR: Constant time equality for undersized Buffers - next attempt. Fixes #2

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 'use strict';
-module.exports = function (a, b) {
+module.exports = function (a, b, minComp) {
 	if (!Buffer.isBuffer(a) || !Buffer.isBuffer(b)) {
 		throw new TypeError('Arguments must be Buffers');
 	}
 
+	var aLen = a.length;
+	var bLen = b.length;
+	var len = Math.max(aLen, bLen, minComp || 0);
 	var ret = 0;
 
-	for (var i = 0; i < a.length; i++) {
-		ret |= a[i] ^ b[i];
+	for (var i = 0; i < len; i++) {
+		ret |= a[i % aLen] ^ b[i % bLen];
 	}
+	ret |= aLen ^ bLen;
 
-	return a.length === b.length && ret === 0;
+	return ret === 0;
 };
--- test.js
+++ test.js
@@ -15,3 +15,27 @@ test(t => {
 	t.throws(() => fn(new Buffer(1), 'abc'));
 	t.end();
 });
+
+test('minComp should not destroy correctness', t => {
+	const a1 = new Buffer('abcde');
+	const a2 = new Buffer('abcde');
+	const b1 = new Buffer('abcdef');
+	const c1 = new Buffer('abcdeg');
+
+	t.true(fn(a1, a2, 0));
+	t.true(fn(a1, a2, a1.length));
+	t.true(fn(a1, a2, a1.length + 1));
+
+	t.false(fn(a1, b1, 0));
+	t.false(fn(a1, b1, a1.length));
+	t.false(fn(a1, b1, a1.length + 1));
+
+	t.false(fn(b1, c1, 0));
+	t.false(fn(b1, c1, b1.length - 1));
+	t.false(fn(b1, c1, b1.length));
+	t.false(fn(b1, c1, b1.length + 1));
+
+	t.true(fn(new Buffer('foo'), new Buffer('foo'), 512));
+
+	t.end();
+});
